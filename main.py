8fb48c1ffc738c6f00701bd2c4881374b2ac0db0
Victor Fernandez <victoraguirre.f@gmail.com>
2021-08-21 02:47:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 
 from secrets import NEWSAPI
 import requests
-
+import json
+import datetime
+import pandas as pd
 
 class News:
 
@@ -80,6 +82,30 @@ class News:
 
 if __name__ == '__main__':
 
+	today = datetime.date.today()
+	yesterday = today - datetime.timedelta(days=1)
+	older = today - datetime.timedelta(days=4)
 	st = News()
-	content = st.fetch_new('wolverine The Long Night')
-	print(f'content \n{content.text}')
+	topic = input("Topic to search: ")
+	content = json.loads(st.fetch_new(q=topic, time_from=older, time_to=today).text)
+	articles = content['articles']
+	#
+	# keys = ['source','author','title','description','url','urlToImage','publishedAt','content']
+
+	# print(f"content \n{articles}")
+	info = {}
+	for article in articles:
+		info['publishedAt'] = article['publishedAt']
+		info['Name'] = article['source']['name']
+		info['title'] = article['title']
+		info['Author'] = article['author']
+		info['description'] = article['description']
+		info['description'] = article['content']
+		# print(f"Name: {article['source']['name']}")
+		# print(f"title: {article['title']}")
+		# print(f"Author: {article['author']}")
+		# print(f"description: {article['description']}")
+		# print(f"content: {article['content']}")
+
+	print(len(info))
+
