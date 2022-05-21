48732aacc1b79ee8003d55ed99e36f3ab477e9ba
Booker <victoraguirre.f@gmail.com>
2022-05-21 12:23:44 +0800

    Adding Menu for domain modification and time frame

--- main.py
+++ main.py
@@ -29,22 +29,23 @@ def configure_logger():
 bot = telebot.TeleBot(token=os.environ['BOTAPIKEY_dev'])
 
 
-# TODO: Allow Client to change time frame.
-today = datetime.date.today()
-older = today - datetime.timedelta(days=4)
-
-
 class MainFilter(telebot.custom_filters.AdvancedCustomFilter):
+	"""Custom filter to be use on message handler with the keyword 'text'"""
 	key = 'text'
 	@staticmethod
 	def check(message, text):
 		logger.debug(f'message comes from the message {message.text} and text come from the decorator {text}')
 		return message.text in text
 
-@bot.message_handler(commands=['start', 'help'])
+
+@bot.message_handler(commands=['start', 'help', 'settings'])
 def send_welcome(message):
 	"""bot start function"""
-	bot.reply_to(message, "Type 'News' follow by a key work, you will get back a list of the latest 3 news")
+	welcome_message = """
+						Type 'News' follow by a key work.\n
+						You will get back a list of the latest 3 news most relevant articles about the topics
+						"""
+	bot.reply_to(message, welcome_message)
 
 
 # validate the keyword, call get_news just if the message has news follow by the keywords
@@ -73,6 +74,9 @@ def bot_get_news(message):
 	text = message.text.split()
 	_, key_words = text[0], text[1:]
 
+	# get timeframe
+	today, older = utilities.get_timeframe()
+
 	# call the object aggregator that contain the topics and the news
 	news = Aggregator(
 		topics_of_interest=key_words,
@@ -90,9 +94,10 @@ def bot_get_news(message):
 
 
 def options_screen() -> types.ReplyKeyboardMarkup:
+	"""Creating the options menu"""
 	markup = types.ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True)
 	options_domains = types.KeyboardButton('Domains')
-	options_old_articles = types.KeyboardButton('time frame')
+	options_old_articles = types.KeyboardButton('Timeframe')
 	options_add_domains = types.KeyboardButton('Add Domains')
 	markup.add(options_domains, options_old_articles, options_add_domains)
 	return markup
@@ -101,19 +106,23 @@ def options_screen() -> types.ReplyKeyboardMarkup:
 @bot.message_handler(text=['options', 'Options', 'option', 'Option'])
 @bot.message_handler(commands=['options', 'Options'])
 def bot_options(message):
+	"""Handle the options request"""
 	markup = options_screen()
 	bot.send_message(chat_id=message.chat.id, text="These are your options:", reply_markup=markup)
 	bot.register_next_step_handler(message=message,callback=select_options)
 
 
 def select_options(message):
-
-	if message.text in ("Domains"):
+	"""selection of the correct option"""
+	if message.text in "Domains":
 		get_domain(message)
-	elif message.text in ("Add Domains"):
+	elif message.text in "Add Domains":
 		request_new_domains(message)
+	elif message.text in "Timeframe":
+		prepare_time_frame(message)
 
 
+@bot.message_handler(commands=['domains', 'domain', 'Domains', 'Domain'])
 def get_domain(message):
 	"""Will get the domains currently use to get the news"""
 	# Keep confusing name domain or sources, because the sources are domains, sub-domain do not work.
@@ -122,21 +131,70 @@ def get_domain(message):
 
 
 def request_new_domains(message):
+	"""Request new domains to the user"""
 	explanation_text = "Add new domain(s) (example of domain forbes.com) more than one domains? use commas."
 	bot.send_message(chat_id=message.chat.id, text=explanation_text)
 	bot.register_next_step_handler(message=message, callback=prepare_new_domain)
 
 
 def prepare_new_domain(message):
-
+	"""Prepare the user provided string to add valid domains to the sources"""
 	domains_to_add,  incorrect_domains = utilities.prepare_new_domains_to_add(message=message)
 	list_domains_to_add = ', '.join(domains_to_add)
 
-	bot.send_message(chat_id=message.chat.id,text=f"added domain(s)\n{list_domains_to_add}")
+	bot.send_message(chat_id=message.chat.id, text=f"added domain(s)\n{list_domains_to_add}")
+
 	if len(incorrect_domains) > 0:
 		bot.send_message(chat_id=message.chat.id,text=f"domain(s) not added\n{', '.join(incorrect_domains)}")
+	# Add the new domains to the list of sources
 	utilities.save_configuration_file(config_key="sources", value=list_domains_to_add)
 
+
+def is_date_change() -> types.ReplyKeyboardMarkup:
+	"""Question if user what to change the timeframe use by the bot to search the news"""
+	markup = types.ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True)
+	options_yes = types.KeyboardButton('YES')
+	options_no = types.KeyboardButton('NO')
+	markup.add(options_yes, options_no)
+	return markup
+
+
+@bot.message_handler(commands=['timeframe', 'Timeframe'])
+def prepare_time_frame(message):
+	"""Report current timeframe set up"""
+	days_old = utilities.read_configuration_file('days_old')
+	explanation_text = f"bot search maximum of {days_old} days old news.\ndo you want to change this number?"
+	markup = is_date_change()
+	bot.send_message(chat_id=message.chat.id, text=explanation_text, reply_markup=markup)
+	bot.register_next_step_handler(message=message, callback=input_date)
+
+
+def input_date(message):
+	"""Request new value for the timeframe"""
+	if message.text in 'YES':
+		bot.send_message(chat_id=message.chat.id, text="input the number")
+		bot.register_next_step_handler(message=message, callback=change_number)
+	else:
+		bot.send_message(chat_id=message.chat.id, text="/start")
+
+
+def change_number(message):
+	"""Change the timeframe"""
+
+	new_date = message.text.strip()
+	original_message = message.message_id
+	if not new_date.isdigit():
+		print(f'it is not a digit {new_date}')
+		bot.send_message(chat_id=message.chat.id, text=f"{new_date} is not a number", reply_to_message_id=original_message)
+		bot.send_message(chat_id=message.chat.id, text="Try again, input a number", reply_to_message_id=original_message)
+		bot.register_next_step_handler(message=message, callback=change_number)
+
+	utilities.save_configuration_file(config_key='days_old',value=new_date)
+	bot.send_message(chat_id=message.chat.id, text=f"{new_date} days set.")
+	bot.send_message(chat_id=message.chat.id, text="/start")
+
+	# Send /start to the chat
+	bot.register_next_step_handler(message=message, callback=send_welcome)
 
 
 #registering the custom filter
@@ -146,6 +204,11 @@ bot.add_custom_filter(MainFilter())
 
 def main():
 	configure_logger()
+
+	#Todo: find a way to read the handle.save
+	# bot.enable_save_next_step_handlers(delay=2)
+	# bot.load_next_step_handlers()
+
 	# keep the bot running
 	bot.infinity_polling()
 
--- utilities.py
+++ utilities.py
@@ -1,8 +1,12 @@
+"""Functions to help the bot, parce, configuration loader """
 import configparser
+import datetime
 import re
 
 
 def get_configuration_file(name, section):
+	"""Get the configuration from configuration file and provide and config object to edit them"""
+
 	config = configparser.ConfigParser()
 	config.read(name)
 	configurations = config[section]
@@ -11,20 +15,28 @@ def get_configuration_file(name, section):
 
 def read_configuration_file(config_key):
 	"""Read the configuration files and get back the value of the key provided"""
+
 	_, configurations = get_configuration_file('configuration.ini', 'NEWS')
 	return configurations[config_key]
 
 
 def save_configuration_file(config_key, value):
 	"""Read the configuration files and get back the value of the key provided"""
+
 	config, configurations = get_configuration_file('configuration.ini', 'NEWS')
 	old_value = read_configuration_file(config_key).replace('"',"")
-	configurations[config_key] = f'"{old_value} , {value}"'
+
+	if config_key in ('days_old'):
+		configurations[config_key] = f'{value}'
+	else:
+		configurations[config_key] = f'"{old_value} , {value}"'
+
 	with open('configuration.ini', 'w') as configfile:
 		config.write(configfile)
 
 
 def prepare_new_domains_to_add(message) -> (list, list):
+	"""Prepare the new domains that are going to be added to the configurations"""
 	list_domains = message.text.split(',')
 	regex = "^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\\.)[A-Za-z]{2,6}"
 	compiled_reg = re.compile(regex)
@@ -40,3 +52,11 @@ def prepare_new_domains_to_add(message) -> (list, list):
 			domains_not_added.append(domain)
 
 	return domains_to_add, domains_not_added
+
+
+def get_timeframe() -> (datetime.datetime, datetime.datetime):
+	"""Get the time frame for the news articles """
+	config_days_old = int(read_configuration_file('days_old'))
+	today = datetime.date.today()
+	older = today - datetime.timedelta(days=config_days_old)
+	return today, older
